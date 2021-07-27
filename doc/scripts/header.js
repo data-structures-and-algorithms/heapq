b9602a6bb4ebf0fd4afb04ba9b48101be8ea2eed
a flying potato <80830782+a-flying-potato@users.noreply.github.com>
2021-07-27 19:24:00 +0200

    :robot: docs: Use codecov as test link.
    These changes were automatically generated by a transform whose code can be found at:
      - https://github.com/make-github-pseudonymous-again/rejuvenate/blob/1b5252aec880aa76850f97b16980a6ce00c782c4/src/transforms/docs:use-codecov-as-test-link.js
    Please contact the author of the transform if you believe there was an error.

--- doc/scripts/header.js
+++ doc/scripts/header.js
@@ -18,7 +18,7 @@ domReady(() => {
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
 	testlink.href =
-		'https://coveralls.io/github/data-structures-and-algorithms/heapq';
+		'https://app.codecov.io/gh/data-structures-and-algorithms/heapq';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
